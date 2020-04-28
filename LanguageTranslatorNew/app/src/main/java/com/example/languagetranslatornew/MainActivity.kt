6630431ff385aaa7688d0a1ac2100be7c63435f3
Stefan <zetksm1@op.ac.nz>
2020-04-28 17:23:13 +1200

    Added exit alert dialog working successfully

--- LanguageTranslatorNew/app/src/main/java/com/example/languagetranslatornew/MainActivity.kt
+++ LanguageTranslatorNew/app/src/main/java/com/example/languagetranslatornew/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,6 +19,29 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        quit.setOnClickListener{
+
+            //define our theme
+            val builder2 = AlertDialog.Builder(this)
+
+            builder2.setTitle("Exit")
+
+            builder2.setIcon(R.drawable.ic_launcher_background)
+
+            builder2.setMessage("Are you sure, you want to exit?")
+
+            builder2.setPositiveButton("Yes") {dialog, which ->
+                finish()
+            }
+
+            builder2.setNegativeButton("No") {dialog, which ->
+                Toast.makeText(this, "You chose not to exit", Toast.LENGTH_LONG).show()
+            }
+            val dialog : AlertDialog = builder2.create()
+
+            dialog.show()
+        }
 
 
 
@@ -46,6 +70,8 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+
 
 
 }
