8f3b161bf3bf9a5f3d7ec02ce19bb02b61d18adf
Stefan <zetksm1@op.ac.nz>
2020-04-29 14:41:17 +1200

    All functionality working with dark and light mode

--- LanguageTranslatorNew/app/src/main/java/com/example/languagetranslatornew/MainActivity.kt
+++ LanguageTranslatorNew/app/src/main/java/com/example/languagetranslatornew/MainActivity.kt
@@ -2,12 +2,14 @@
 package com.example.languagetranslatornew
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -98,6 +100,39 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
+
+
+
+        var appSettingsPrefs: SharedPreferences = getSharedPreferences("AppSettingPrefs", 0)
+        val isNightModeOn: Boolean = appSettingsPrefs.getBoolean("NightMode", false)
+        val sharedPrefsEdit: SharedPreferences.Editor = appSettingsPrefs.edit()
+
+        if(isNightModeOn)
+        {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            switchBtn.text = "Disable Dark Mode"
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            switchBtn.text = "Enable Dark Mode"
+        }
+
+        switchBtn.setOnClickListener(View.OnClickListener {
+            if(isNightModeOn){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                sharedPrefsEdit.putBoolean("NightMode", false)
+                sharedPrefsEdit.apply()
+
+                switchBtn.text = "Enable Dark Mode"
+
+            }else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                sharedPrefsEdit.putBoolean("NightMode", true)
+                sharedPrefsEdit.apply()
+
+                switchBtn.text = "Disable Dark Mode"
+            }
+
+        })
 
     }
 
